ecad8b87aded262aa934039e623beca4b918c012
Ilya Roslik <ilya.roslik@sanepid.gov.pl>
2023-10-28 21:04:24 +0200

    Change project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/modules-template'),
+      dir: require('path').join(__dirname, './coverage/angular-project-template'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }]
     },
